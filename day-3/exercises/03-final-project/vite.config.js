3a9d2514be046dd9a666003e774025c009c47ed8
dannycahyo <dannydwicahyono@gmail.com>
2025-11-23 08:37:05 +0700

    chore: Update Vite configuration roots for day 3 exercise and solution

--- day-3/exercises/03-final-project/vite.config.js
+++ day-3/exercises/03-final-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: './exercises/03-final-project',
   server: {
     port: 3007,
   },
--- day-3/solutions/02-data-fetching/vite.config.js
+++ day-3/solutions/02-data-fetching/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: './solutions/02-data-fetching',
   server: {
     port: 3016,
   },
